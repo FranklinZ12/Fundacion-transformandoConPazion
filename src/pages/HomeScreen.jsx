a791e18b42d9e6549a62cd652a067f1dcbd6ec7a
FranklinZ12 <franklinq8@gmail.com>
2022-05-11 17:13:41 -0500

    banner Home terminado

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,18 +1,19 @@
 import 'styles/home/home.css';
-import image from 'assets/TCP.webp';
+import image from 'assets/ClubDeportivoTCP.png';
 
 const HomeScreen = () => {
   return (
     <section>
       <figure className='datos'>
-        <div className='mt-14'>
-          <figcaption>
-            <h1 className='text-5xl mb-3'>Fundacion transformando con pazión</h1>
-            <span className='text-base mb-5 text-justify'>Confianza y transformación social</span>
-          </figcaption>
-        </div>
-        <div className='imagen'>
+        <div className='imagen mt-14'>
           <img src={image} alt='imagen-fundacion' />
+        </div>
+        <div>
+          <figcaption className='text-center'>
+            <h1 className='text-5xl font-medium color-text'>Transformando <br />Con <span>pazión</span></h1>
+            <h3 className='text-3xl font-bold my-2 color-text'>FUNDACIÓN</h3>
+            <p className='text-base opacity-100 text-gray-600'>Confianza y transformación social</p>
+          </figcaption>
         </div>
       </figure>
     </section>
