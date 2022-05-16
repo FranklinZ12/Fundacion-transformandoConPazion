45f03d2bf119b9e27142458b2276560b0385acf6
FranklinZ12 <franklinq8@gmail.com>
2022-05-16 17:35:35 -0500

    card principios page about terminada

--- src/pages/about/AboutScreen.jsx
+++ src/pages/about/AboutScreen.jsx
@@ -1,7 +1,71 @@
+import 'styles/about/about.css';
 
 const AboutScreen = () => {
   return (
-    <div>AboutScreen</div>
+    <section className='aboutScreen'>
+      <div className='flex flex-col text-center'>
+        <h2 className='font-semibold text-5xl mb-6 text-gray-800'>Principios Corporativos</h2>
+        <p>Nuestros principios como fundación</p>
+        <div className='w-full flex flex-wrap gap-6'>
+          <div className='card-About'>
+            <figure className='text-left'>
+              <div className='icon'>
+                <i className="fa-solid fa-dove icon-style" role='img' title='paloma' />
+              </div>
+              <figcaption>
+                <h3 className='my-3'>Garantía de derechos</h3>
+                <h4>Por medio del comportamiento de los jóvenes y grupos sociales,
+                  como fundación se les respetara sus derechos, custodiando día a día
+                  que estos sean cumplidos.
+                </h4>
+              </figcaption>
+            </figure>
+          </div>
+          <div className='card-About'>
+            <figure className='text-left'>
+              <div className='icon'>
+                <i className="fa-solid fa-dove icon-style" role='img' title='paloma' />
+              </div>
+              <figcaption>
+                <h3 className='my-3'>Garantía de derechos</h3>
+                <h4>Por medio del comportamiento de los jóvenes y grupos sociales,
+                  como fundación se les respetara sus derechos, custodiando día a día
+                  que estos sean cumplidos.
+                </h4>
+              </figcaption>
+            </figure>
+          </div>
+          <div className='card-About'>
+            <figure className='text-left'>
+              <div className='icon'>
+                <i className="fa-solid fa-dove icon-style" role='img' title='paloma' />
+              </div>
+              <figcaption>
+                <h3 className='my-3'>Garantía de derechos</h3>
+                <h4>Por medio del comportamiento de los jóvenes y grupos sociales,
+                  como fundación se les respetara sus derechos, custodiando día a día
+                  que estos sean cumplidos.
+                </h4>
+              </figcaption>
+            </figure>
+          </div>
+          <div className='card-About'>
+            <figure className='text-left'>
+              <div className='icon'>
+                <i className="fa-solid fa-dove icon-style" role='img' title='paloma' />
+              </div>
+              <figcaption>
+                <h3 className='my-3'>Garantía de derechos</h3>
+                <h4>Por medio del comportamiento de los jóvenes y grupos sociales,
+                  como fundación se les respetara sus derechos, custodiando día a día
+                  que estos sean cumplidos.
+                </h4>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
